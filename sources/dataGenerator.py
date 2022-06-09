24664cdd515647583805dd25c48ea69a27ee5c26
Sebastian Bran <sebastianbranzapata@gmail.com>
2022-06-08 23:01:41 -0500

    Close #11

--- sources/dataGenerator.py
+++ sources/dataGenerator.py
@@ -24,6 +24,9 @@ class Graph:
 
       if adjacencyList.get(source) is None:
         adjacencyList[source] = []
+      if adjacencyList.get(target) is None:
+        adjacencyList[target] = []
+
       adjacencyList[source].append(target)
     
     return mapOfEdges, adjacencyList
@@ -35,23 +38,22 @@ def initGraph(documentPath):
   graph = Graph(data["nodes"], data["links"])
   return graph
 
-graph = initGraph("calles_san_juan_de_lurigancho.json")
+graph = initGraph("data/calles_san_juan_de_lurigancho.json")
 
-'''with open("adjacency_list.json", mode="w", encoding="utf-8") as f:
+with open("adjacency_list.json", mode="w", encoding="utf-8") as f:
   adjacencyList = json.dumps(graph.adjacencyList)
   f.write(adjacencyList)
-'''
 
-def remap_keys(edges):
+'''def remap_keys(edges):
   return [{"key": k, "value": v} for k, v in edges.items()]
 
 with open("calles.json", mode="w", encoding="utf-8") as f:
   calles = json.dumps(remap_keys(graph.edges))
-  f.write(calles)
+  f.write(calles)'''
 
-with open("intersecciones.json", mode="w", encoding="utf-8") as f:
+'''with open("intersecciones.json", mode="w", encoding="utf-8") as f:
   intersecciones = json.dumps(graph.nodes)
-  f.write(intersecciones)
+  f.write(intersecciones)'''
 
 #print(graph.nodes[181285635])
 #print(graph.adjacencyList[5723856941])
@@ -60,10 +62,10 @@ with open("intersecciones.json", mode="w", encoding="utf-8") as f:
 #print(graph.nodes[1342139130])
 #print(graph.edges[(8231346943, 1342139130)])
 
-node_ini = 8231346943
+'''node_ini = 8231346943
 print(graph.adjacencyList[node_ini])
 for node in graph.adjacencyList[node_ini]:
-  print(graph.edges[(node_ini, node)])
+  print(graph.edges[(node_ini, node)])'''
 #print(graph.nodes[5723856944])
 #print(graph.edges[(5723856941, 5723856944)])
 #print(graph.nodes)
--- sources/perlinGenerator.py
+++ sources/perlinGenerator.py
@@ -1,13 +1,43 @@
-import cityGraph as gp
+import json
+import math
+from cityGraph import CityGraph
+from perlin_noise import PerlinNoise
 
-def bfs(graph: gp.CityGraph):
-  seen = { k: False for k, v in graph.adjacencyList.items() }
+def generatePerlinNoise(n):
+  noise = PerlinNoise(octaves = 8, seed = 1234)
+  xd, yd = n * 5, 1
+  pnoise = [[noise([i/xd, j/yd]) for i in range(xd)] for j in range(yd)]
+  
+  values = []
+  mini = math.inf
+  maxi = -math.inf
 
-  finish = False
-  cont = 0
+  for v in pnoise:
+    for e in v:
+      e = int(e * 100)
+      mini = min(mini, e)
+      maxi = max(maxi, e)
+      values.append(e)
+
+  if mini < 0:
+    values = list(map(lambda x: x + abs(mini), values))
+
+  diff = maxi - mini
+  convertor = diff / 50
+
+  values = list(map(lambda x: int(x / convertor), values))
+  return values
+
+def bfs(graph: CityGraph, perNoise):
+  seen = {}
+  for k, v in graph.adjacencyList.items():
+    for e in v:
+      seen[(k, e)] = False
+
+  indexPer = 0
 
   def bfsUtil(node):
-    nonlocal cont
+    nonlocal indexPer
 
     queue = []
 
@@ -15,22 +45,47 @@ def bfs(graph: gp.CityGraph):
     seen[node] = True
 
     while queue:
-      if cont > 50:
-        break
       curr_node = queue.pop(0)
-      for child_node in graph.adjacencyList[curr_node]:
-        child_node = str(child_node)
-        if not seen[child_node]:
-          seen[child_node] = True
-          print(curr_node, child_node)
+      for child_node in graph.adjacencyList[str(curr_node)]:
+        if not seen[(str(curr_node), child_node)]:
+          seen[(str(curr_node), child_node)] = True
           queue.append(child_node)
-          cont += 1
 
-  for k in graph.adjacencyList.keys():
-    if finish:
+          graph.streets[(int(curr_node), int(child_node))].update({"val": perNoise[indexPer]})
+
+          indexPer += 1
+
+  for k, v in graph.adjacencyList.items():
+    for e in v:
+      if not seen[(k, e)]:
+        bfsUtil(k)
+
+adjacencyListPath = "data/adjacency_list.json"
+streetsPath = "data/calles.json"
+intersectionsPath = "data/intersecciones.json"
+
+cityGraph = CityGraph(adjacencyListPath=adjacencyListPath, streetsPath=streetsPath, intersectionsPath=intersectionsPath)
+perNoise = generatePerlinNoise(len(cityGraph.streets))
+
+bfs(cityGraph, perNoise)
+
+isSucces = True
+for v in cityGraph.streets.values():
+  try:
+    if v["val"] is not None:
       break
-    if not seen[k]:
-      bfsUtil(k)
+  except:
+    isSucces = False
+    continue
 
+def remap_keys(edges):
+    return [{"key": k, "value": v} for k, v in edges.items()]
 
-#bfs(cityGraph)
+if isSucces:
+  print("exito :)")
+  
+  with open("calles.json", mode="w", encoding="utf-8") as f:
+    streets = json.dumps(remap_keys(cityGraph.streets))
+    f.write(streets)
+else:
+  print("F")
