0193c4dca133850eb5f748b8e234d77b4a54cd57
thecatuniverse <55470371+thecatuniverse@users.noreply.github.com>
2022-06-25 16:10:28 -0500

    Close #15

--- interfaz/adjacencyListGenerator.py
+++ interfaz/adjacencyListGenerator.py
@@ -1,30 +1,46 @@
 from datetime import datetime
-
+import json
 import sys
-sys.path.insert(1, 'C:/Users/User/Documents/personal-projects/complejidad/TF-201720651-201920080-201920151/sources') #path of the folder where the cityGraph is saved
-from cityGraph import CityGraph
+
+#sys.path.insert(1, '../sources')  # path of the folder where the cityGraph is saved
+from sources.cityGraph import CityGraph
+
 
 def generateAdjacencyListForUI(cityGraph: CityGraph):
-  curr_time = datetime.now().hour
+    curr_time = datetime.now().hour
 
-  def getWeight(city1, city2):
-    street = cityGraph.streets[(city1, city2)]
-    val = street["val"]
-    length = street["length"]
-    return cityGraph.calculate_weight(val, length, curr_time)
+    def getWeight(city1, city2):
+        street = cityGraph.streets[(city1, city2)]
+        val = street["val"]
+        length = street["length"]
+        return cityGraph.calculate_weight(val, length, curr_time)
 
-  new_adjacencyList = {}
-  for key, arr in cityGraph.adjacencyList.items():
-    key = int(key)
-    new_adjacencyList[key] = list(map(lambda x: (x, getWeight(key, x)), arr))
+    new_adjacencyList = {}
+    for key, arr in cityGraph.adjacencyList.items():
+        key = int(key)
+        new_adjacencyList[key] = list(map(lambda x: (x, getWeight(key, x)), arr))
 
-  return new_adjacencyList
+    return new_adjacencyList
 
-adjacencyListPath = "data/adjacency_list.json"
-streetsPath = "data/calles.json"
-intersectionsPath = "data/intersecciones.json"
+
+def generateLocationListForUI(intersectionsPath):
+    with open(intersectionsPath) as file:
+        data = json.load(file)
+
+    transform = dict()
+    for i in data:
+        transform[int(i)] = (data[i]['y'], data[i]['x'])
+
+    return transform
+
+
+adjacencyListPath = "../data/adjacency_list.json"
+streetsPath = "../data/calles.json"
+intersectionsPath = "../data/intersecciones.json"
 
 cityGraph = CityGraph(adjacencyListPath=adjacencyListPath, streetsPath=streetsPath, intersectionsPath=intersectionsPath)
 
 new_adjacencyList = generateAdjacencyListForUI(cityGraph)
+locationsList = generateLocationListForUI(intersectionsPath)
 print(new_adjacencyList)
+# print(locationsList)
